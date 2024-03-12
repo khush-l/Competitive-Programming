ab07335030c179069192f162e18c2d15d2a739f4
khush-l <82348535+khush-l@users.noreply.github.com>
2024-03-11 22:33:25 -0500

    Add files via upload

---
+++ CSES_questions/Counting_Divisors .cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int N; cin >> N;
+	for (int i=0; i<N;i++) {
+		int count=0;
+		int x;
+		cin >> x;
+		for (int y=1;y*y<=x;y++) {
+			if (x % y == 0) { 
+				count += (y * y == x ? 1 : 2); 
+				}
+		}
+		cout << count<<endl;
+	}
+}
